ee07cbb682723e11206b1cb6d29f00277118e221
Electrotips <boudjelalhd@gmail.com>
2026-03-28 22:12:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10795648
+    "domain": "5gvci.com",
+    "zoneId": 10799755
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
